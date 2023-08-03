402155510e0c446738bd9a4bb3e6d314322f103c
Nkugwa Mark William <53256592+MarkNwilliam@users.noreply.github.com>
2023-08-04 02:32:15 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,27 +1,40 @@
 #!/usr/bin/python3
+"""Lockboxes Contains method that finds the keys to
+open other lockboxes
+"""
+
 def canUnlockAll(boxes):
     """
-    Determines if all the boxes can be opened
+    Function that determines if you can open all the lockboxes
+    Args:
+        boxes: list of lists of integers
+    Returns:
+        True if you can open all the lockboxes, False otherwise
     """
-    # List to keep track of unlocked boxes
-    unlocked = [False] * len(boxes)
-    # Mark the first box as unlocked
-    unlocked[0] = True
-    
-    # Keys we have
-    keys = [0]
+    # Set to keep track of boxes we've unlocked or visited
+    visited = set()
 
-    while keys:
-        current_key = keys.pop()
+    # Queue for BFS approach (starts with box 0)
+    queue = [0]
 
-        # Unlocking boxes using current key
-        for key in boxes[current_key]:
-            if key < len(boxes) and not unlocked[key]:
-                unlocked[key] = True
-                keys.append(key)
-    # If any box remains locked, return False, otherwise True
-    return all(unlocked)
+    while queue:
+        current_box = queue.pop(0)
+        # If we've already processed this box, continue to the next one
+        if current_box in visited:
+            continue
 
+        visited.add(current_box)
+        
+        for key in boxes[current_box]:
+            # Only add the key to the queue if it's a valid box number
+            # and hasn't been visited yet
+            if key < len(boxes) and key not in visited:
+                queue.append(key)
+
+    # If we've visited all the boxes, return True
+    return len(visited) == len(boxes)
+
+# Sample Test Cases
 if __name__ == "__main__":
     boxes = [[1], [2], [3], [4], []]
     print(canUnlockAll(boxes))
@@ -31,3 +44,4 @@ if __name__ == "__main__":
 
     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
     print(canUnlockAll(boxes))
+
