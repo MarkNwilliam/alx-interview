549c23ff973a7547f95e7f90174b47ea387040ef
mac <mac@macs-MacBook-Pro.local>
2023-08-11 01:03:48 +0300

    interviw

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Minimum operations question.
+'''
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+    
+    # Break down the given number n into its prime factors
+    # The sum of those prime factors gives the minimum operations required
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n = n // divisor
+        divisor += 1
+
+    return operations
